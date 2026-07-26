9f8efe873bcee4ce73fb50b4a00b5fe381c7583b
dorisaward <dorisaward@gmail.com>
2026-07-26 08:40:42 +0100

    Attempt 2

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
-import { registerRootComponent } from 'expo'
+import { BodyText } from './components/BodyText'
+import { Container } from './components/Container'
+import { Heading, HeadingProps } from './components/Heading'
+import { ThemeToggle } from './components/implementedComponents/ThemeToggle'
+import { Toggle, ToggleProps } from './components/Toggle'
+import { useTheme, ThemeProvider } from './theme/useTheme'
 
-import App from './App'
+export {
+	BodyText,
+	Container,
+	Heading,
+	HeadingProps,
+	ThemeProvider,
+	ThemeToggle,
+	Toggle,
+	ToggleProps,
+	useTheme,
+}
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App)
+// import { registerRootComponent } from 'expo'
+//
+// import App from './App'
+//
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in Expo Go or in a native build,
+// // the environment is set up appropriately
+// registerRootComponent(App)
